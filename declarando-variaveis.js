e5b908c9c43e5d086e9f99d78b5a326b14cc0886
Ivan Forner <ivanforner@gmail.com>
2022-05-05 15:46:24 -0300

    aula de variaveis

---
+++ declarando-variaveis.js
@@ -0,0 +1,18 @@
+// var
+
+var altura = 5;
+var comprimento = 7;
+
+var area = altura * comprimento;
+
+console.log(area);
+
+
+// Let
+
+let altura2 = 10;
+let comprimento2 = 2.8;
+
+let area2 = altura2 * comprimento2;
+
+console.log(area2);
