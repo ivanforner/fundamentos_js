33d61597706d58ac23a475774fc02c3d6ce94b99
Ivan Forner <ivanforner@gmail.com>
2022-05-09 09:26:00 -0300

    Aula de comparações

---
+++ comparacoes.js
@@ -0,0 +1,14 @@
+// == (comparação implicita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+
+// === (comparação explícita)
+
+console.log(numero === texto);
+
+console.log(typeof(numero));
+console.log(typeof(texto));
